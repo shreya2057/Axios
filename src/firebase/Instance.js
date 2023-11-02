992768b789540c1ed9875cfc71b4aab69ccddeed
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-02 21:07:41 +0545

    Axios instance created

---
+++ src/firebase/Instance.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+// axios.create() -> Method in Axios library used to create new instance
+//                   of Axios.
+//                -> axios.create({
+//                      baseURL: url, 
+//                      timeout: time,
+//                      headers: {headers}
+//                   })
+//                      -> baseURL=: Sets base Url for all requests made 
+//                                   through this instance.
+//                      -> timeout=: Sets default timeout for request made
+//                                   through this instance.
+//                      -> headers=: It allows to define default headers for
+//                                   the requests made using this instance. 
+//                                   Common headers such as Authorization 
+//                                   or Content-Type can be set here.
+
+const url = "https://axios-43afa-default-rtdb.firebaseio.com/";
+
+const axios_instance = axios.create({
+    baseURL: {url}
+});
+
+export {axios_instance}
